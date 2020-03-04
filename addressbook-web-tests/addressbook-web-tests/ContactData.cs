9f597ea6407113b6377d79ecc9768eb28189cd20
Игорь Пронин <igor.pronin1988@gmail.com>
2020-03-05 01:19:55 +0300

    Добавлен тест для контактов и разбито на вспомогательные методы

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressbookTest
+{
+    class ContactData
+    {
+        private string name;
+        private string middlename = "";
+        private string lastname = "";
+
+        public ContactData(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string MiddleName
+        {
+            get
+            {
+                return middlename;
+            }
+            set
+            {
+                middlename = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+    }
+}
